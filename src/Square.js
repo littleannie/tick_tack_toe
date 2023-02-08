3df92f06e758dc7db2c6109608b00f8ad873f658
Anne Ly <anne.ly59@gmail.com>
2023-02-08 16:46:50 +0100

    tick tack

---
+++ src/Square.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { useState } from 'react';
+
+export default function Square() {
+  const [value, setValue] = useState(null);
+
+
+  function handleClick() {
+    setValue('X');
+  }
+
+  return (
+    <button
+      className="square"
+      onClick={handleClick}
+    >
+      {value}
+    </button>
+  );
+}
