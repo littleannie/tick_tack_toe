8e2dcc87531a76ac0edee898581e2071ef1be0ec
Anne Ly <anne.ly59@gmail.com>
2023-02-09 10:48:19 +0100

    tick tack toe return

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ export default function Board() {
 
   function handleClick(i) {
     const nextSquares = squares.slice();
+    if (squares[i]) {
+      return;
+    }
     if (xIsNext) {
       nextSquares[i] = "X";
       debugger;
